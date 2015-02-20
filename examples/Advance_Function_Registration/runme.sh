2dfb4d021b696f9e9fca57f2eb9e2a60d2955c27
chinmay2312 <chinmay2312@gmail.com>
2015-02-20 19:36:04 +0530

    * Adding advance function registration example 2

--- examples/Advance_Function_Registration/runme.sh
+++ examples/Advance_Function_Registration/runme.sh
@@ -2,4 +2,8 @@ echo "Compiling example1..."
 g++ -std=c++11 example1.cpp -o example1
 echo "Running example1..."
 ./example1
+echo "Compiling example2..."
+g++ -std=c++11 example2.cpp -o example2
+echo "Running example2..."
+./example2
 
